d3c04f4e057411000a4d3431a0aa137a7f93bffc
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-01-31 22:49:08 +0900

    feat: add site image

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,9 +3,11 @@ const seo = () => (
     <title>Coronavirus Infection Status</title>
     <meta name='description' content='Up-to-date coronavirus infection status' />
     <meta name='keywords' content='coronavirus, updates, infection status, confirmed cases, deaths, recovered' />
+    <meta name='image' content='https://coronavirus-info.now.sh/site-image.png' />
     <meta property='og:url' content='https://coronavirus-info.now.sh/' />
     <meta property='og:title' content='Coronavirus Infection Status' />
     <meta property='og:description' content='Up-to-date coronavirus infection status' />
+    <meta property='og:image' content='https://coronavirus-info.now.sh/site-image.png' />
     <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
     <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
     <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
