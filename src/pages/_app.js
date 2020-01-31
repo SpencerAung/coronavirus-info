df5adbe173f8b62b04724d620847a60b87bffb26
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-01-31 18:33:55 +0900

    feat: add github link

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,9 @@ export default function MyApp ({ Component, pageProps }) {
   return (
     <div style={{ margin: '20rem' }}>
       <Component {...pageProps} />
+      <footer style={{ textAlign: 'center', margin: '50rem' }}>
+        <a href='https://github.com/SpencerAung/coronavirus-info' target='blank'>github</a>
+      </footer>
     </div>
   )
 }
