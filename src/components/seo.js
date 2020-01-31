fb130503f5dfacda2ea073901b77f36bff1a4d44
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-01-31 19:01:24 +0900

    feat: add seo meta tags

---
+++ src/components/seo.js
@@ -0,0 +1,12 @@
+const seo = () => (
+  <>
+    <title>Coronavirus Infection Status</title>
+    <meta name='description' content='Up-to-date coronavirus infection status' />
+    <meta name='keywords' content='coronavirus, updates, infection status, confirmed cases, deaths, recovered' />
+    <meta property='og:url' content='https://coronavirus-info.now.sh/' />
+    <meta property='og:title' content='Coronavirus Infection Status' />
+    <meta property='og:description' content='Up-to-date coronavirus infection status' />
+  </>
+)
+
+export default seo
