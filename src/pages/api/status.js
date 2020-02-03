1aed893a9342d7c8c5356aebf8459ea4a853379d
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-02-03 14:08:29 +0900

    feat: add status api endpoint

---
+++ src/pages/api/status.js
@@ -0,0 +1,37 @@
+const Airtable = require('airtable')
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+})
+
+const base = Airtable.base('appwCqMJOK5qf6l6l')
+
+const fetchStatus = () => {
+  return new Promise((resolve, reject) => {
+    base('StatusByCountry').select({
+      view: 'Grid view'
+    }).firstPage(function (err, records) {
+      if (err) {
+        reject(err)
+      }
+      resolve(records)
+    })
+  })
+}
+
+const getFields = fetchedStatuses => fetchedStatuses.map(status => ({ ...status.fields }))
+
+export default async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const statuses = await fetchStatus()
+
+      res.send(getFields(statuses))
+    } catch (e) {
+      console.error(e)
+      res.send({ error: e })
+    }
+  } else {
+    res.send(`${req.method} route does not exist`)
+  }
+}
