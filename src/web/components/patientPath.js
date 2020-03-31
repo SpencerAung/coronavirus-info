f9b7528b899778158f1806e324839eab9da32baf
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-03-31 18:11:22 +0900

    feat: support patient list pagination

--- src/web/components/patientPath.js
+++ src/web/components/patientPath.js
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled'
-import { useState } from 'react'
-import { IoIosArrowDropdownCircle as ArrowDown, IoIosArrowDropupCircle as ArrowUp } from 'react-icons/io'
-
-import useApi from '../hooks/useApi'
+import fetch from 'node-fetch'
+import { useState, useEffect, useRef, useReducer } from 'react'
+import { IoIosArrowDropdownCircle as ArrowDown, IoIosArrowDropupCircle as ArrowUp, IoIosArrowForward as ArrowNext, IoIosArrowBack as ArrowPrev } from 'react-icons/io'
+import Spinner from './spinner'
 
 const PatientWrapper = styled.div`
   padding: 10rem;
@@ -28,6 +28,25 @@ const PatientTitle = styled.div`
 
   .recovered {
     color: ${props => props.theme.colors.green};
+  }
+`
+const PaginationWrapper = styled.div`
+  margin-top: 20rem;
+  text-align: center;
+
+
+  button {
+    padding: 8rem;
+    margin: 0 10rem;
+    color: ${props => props.theme.colors.purple};
+    background-color: transparent;
+    font-size: 30rem;
+    text-align: center;
+    border: none;
+
+    &:disabled {
+      color: ${props => props.theme.colors.offWhite};
+    }
   }
 `
 
@@ -61,14 +80,74 @@ const PatientItem = ({ patientId, status, nationality, age, gender, notes, hospi
   )
 }
 
+const initialState = { records: [], page: 1, isEnd: null }
+
+function reducer (state, action) {
+  switch (action.type) {
+    case 'NEW_RECORDS':
+      return { ...state, records: [...state.records, ...action.payload] }
+    case 'NEXT_PAGE':
+      return { ...state, page: state.page + 1 }
+    case 'PREV_PAGE':
+      return { ...state, page: state.page - 1 }
+    case 'END_OF_RECORDS':
+      return { ...state, isEnd: true }
+    default:
+      return state
+  }
+}
+
+const PAGE_SIZE = 10
+const fetchPatientRecords = async (pageSize = PAGE_SIZE, offset) => {
+  const url = offset ? `/api/myanmar-patients?offset=${offset}&pageSize=${pageSize}` : `/api/myanmar-patients?pageSize=${pageSize}`
+  const fetched = await fetch(url)
+  const result = await fetched.json()
+
+  return result
+}
+
 const PatientPath = () => {
-  const patients = useApi('/api/myanmar-patients', [])
-  console.log(patients)
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const pageOffset = useRef(null)
+
+  const handleNextButtonClick = () => {
+    if (!state.isEnd) {
+      fetchRecords()
+    }
+
+    dispatch({ type: 'NEXT_PAGE' })
+  }
+  const handlePrevButtonClick = () => dispatch({ type: 'PREV_PAGE' })
+  const fetchRecords = async () => {
+    const { records, offset } = await fetchPatientRecords(PAGE_SIZE, pageOffset.current)
+
+    pageOffset.current = offset
+
+    dispatch({ type: 'NEW_RECORDS', payload: records })
+
+    if (!offset) {
+      dispatch({ type: 'END_OF_RECORDS' })
+    }
+  }
+
+  useEffect(() => {
+    fetchRecords()
+  }, [])
+
+  const { records, isEnd, page } = state
+  const start = PAGE_SIZE * (page - 1)
+  const patients = records.slice(start, start + PAGE_SIZE)
+  const isLastPage = isEnd && records.length <= page * PAGE_SIZE
 
   return (
     <div style={{ marginBottom: '80px' }}>
       <h1 className='mm-font'>ပိုးတွေ့လူနာများ</h1>
+      {!patients.length && <Spinner />}
       {patients.map(patient => <PatientItem key={patient.id} {...patient} />)}
+      <PaginationWrapper>
+        <button disabled={page <= 1} onClick={handlePrevButtonClick}><ArrowPrev /></button>
+        <button disabled={isLastPage} onClick={handleNextButtonClick}><ArrowNext /></button>
+      </PaginationWrapper>
     </div>
   )
 }
--- src/web/pages/api/myanmar-patients.js
+++ src/web/pages/api/myanmar-patients.js
@@ -1,35 +1,28 @@
-const Airtable = require('airtable')
-
-Airtable.configure({
-  endpointUrl: 'https://api.airtable.com',
-  apiKey: process.env.AIRTABLE_API_KEY
-})
-
-const base = Airtable.base('appYR4mmID6oPVAlx')
-
-const fetchStatus = () => {
-  return new Promise((resolve, reject) => {
-    base('PatientPath').select({
-      view: 'Grid view'
-    }).firstPage(function (err, records) {
-      if (err) {
-        reject(err)
-      }
-      resolve(records)
-    })
-  })
-}
+const fetch = require('node-fetch')
 
 const getFields = fetchedStatuses => fetchedStatuses.map(status => ({ ...status.fields }))
 
+// https://codepen.io/airtable/full/rLKkYB
+const URL = 'https://api.airtable.com/v0/appYR4mmID6oPVAlx/PatientPath?sort%5B0%5D%5Bfield%5D=patientId&sort%5B0%5D%5Bdirection%5D=desc'
+const OPTIONS = {
+  headers: {
+    Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+  }
+}
+
 export default async (req, res) => {
   if (req.method === 'GET') {
+    const { offset = null, pageSize = 10 } = req.query
+
     try {
-      const statuses = await fetchStatus()
-      res.send(getFields(statuses))
+      const urlWithOffset = offset ? `${URL}&offset=${offset}&pageSize=${pageSize}` : `${URL}&pageSize=${pageSize}`
+      const fetched = await fetch(urlWithOffset, OPTIONS)
+      const statuses = await fetched.json()
+
+      res.send({ records: getFields(statuses.records), offset: statuses.offset })
     } catch (e) {
       console.error(e)
-      res.send({ error: e })
+      res.send({ error: e, records: [] })
     }
   } else {
     res.send(`${req.method} route does not exist`)
