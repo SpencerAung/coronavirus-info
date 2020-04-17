985b59015a67e738326b3d1f29d7625193aab877
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-04-18 04:23:04 +0900

    feat: update myanmar news api

--- src/web/components/myanmarNews.js
+++ src/web/components/myanmarNews.js
@@ -1,14 +1,24 @@
 import NewsItem from './newsItem'
 import useApi from '../hooks/useApi'
 
+import { getFormattedDate } from '../helpers'
 const MyanmarNews = () => {
-  const news = useApi('/api/myanmar-news', [])
+  const dateStr = getFormattedDate(new Date())
+  const { items = [] } = useApi(`https://due79jm6nb.execute-api.ap-southeast-1.amazonaws.com/v1/news/covid-19?pageSize=30&from=${dateStr}`, {})
+  const news = items.map(item => ({
+    url: item.url,
+    title: item.title,
+    id: item.id,
+    source: (item.siteName || '').startsWith('7day') ? '7day News' : item.siteName,
+    date: item.publishedDate
+  })
+  )
 
   return (
     <div>
       <h1 className='mm-font'>သတင်း</h1>
       {!news.length && <p>Fetching Myanmar news...</p>}
-      {news.map(item => <NewsItem titleClassName='mm-font' key={item.url} {...item} />)}
+      {news.map(item => <NewsItem titleClassName='mm-font' key={item.id} {...item} />)}
     </div>
   )
 }
