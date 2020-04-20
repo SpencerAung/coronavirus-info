4537730bd4714bbe3d427e9c34ec9782b746829c
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-04-20 11:47:14 +0900

    feat: separate today and yesterday news

--- src/web/components/myanmarNews.js
+++ src/web/components/myanmarNews.js
@@ -1,27 +1,49 @@
 import NewsItem from './newsItem'
+import PropTypes from 'prop-types'
 import useApi from '../hooks/useApi'
 
-import { getFormattedDate } from '../helpers'
-const MyanmarNews = () => {
-  const tmrw = new Date().setDate(new Date().getUTCDate() + 1)
-  const dateStr = getFormattedDate(new Date(tmrw))
-  const { items = [] } = useApi(`https://due79jm6nb.execute-api.ap-southeast-1.amazonaws.com/v1/news/covid-19?pageSize=30&from=${dateStr}`, {})
+const API_URL = 'https://due79jm6nb.execute-api.ap-southeast-1.amazonaws.com/v1/news/covid-19'
+const getNewsApiUrl = params => {
+  return API_URL + '?' + Object.keys(params).map((key) =>
+    `${key}=${params[key]}`).join('&')
+}
+const MyanmarNews = ({ title, pageSize, from, to }) => {
+  const url = getNewsApiUrl({ pageSize, from, to })
+
+  const { items = [] } = useApi(url, {})
   const news = items.map(item => ({
     url: item.url,
     title: item.title,
     id: item.id,
-    source: (item.siteName || '').startsWith('7day') ? '7day News' : item.siteName,
+    source: item.siteName,
     date: item.publishedDate
   })
   )
 
+  if (news.length === 0) {
+    return null
+  }
+
   return (
     <div>
-      <h1 className='mm-font'>သတင်း</h1>
-      {!news.length && <p>Fetching Myanmar news...</p>}
+      <h1 className='mm-font'>{title}</h1>
       {news.map(item => <NewsItem titleClassName='mm-font' key={item.id} {...item} />)}
     </div>
   )
+}
+
+MyanmarNews.propTypes = {
+  title: PropTypes.string,
+  pageSize: PropTypes.number,
+  from: PropTypes.string,
+  to: PropTypes.string
+}
+
+MyanmarNews.defaultProps = {
+  title: '',
+  pageSize: 20,
+  from: '',
+  to: ''
 }
 
 export default MyanmarNews
--- src/web/pages/myanmar-news.js
+++ src/web/pages/myanmar-news.js
@@ -2,12 +2,19 @@ import MyanmarNews from '../components/myanmarNews'
 import MyanmarStatus from '../components/myanmarStatus'
 import PatientPath from '../components/patientPath'
 
+import { getFormattedDate } from '../helpers'
+
 const MyanmarNewsPage = () => {
+  const todayDateStr = getFormattedDate(new Date())
+  const yesterday = new Date().setDate(new Date().getUTCDate() - 1)
+  const yesterdayDateStr = getFormattedDate(new Date(yesterday))
+
   return (
     <div>
       <MyanmarStatus />
       <PatientPath />
-      <MyanmarNews />
+      <MyanmarNews title='Today News' pageSize={100} from={todayDateStr} to={todayDateStr} />
+      <MyanmarNews title='Yesterday News' pageSize={100} from={yesterdayDateStr} to={yesterdayDateStr} />
     </div>
   )
 }
